bf07dbe781733776b3bdf586ad394a0283bef811
Alexander Baransky <alexander.baranskiy@yandex.ru>
2023-11-24 14:42:37 +0300

    Add /delete_user endpoint

--- endpoints/admin.cpp
+++ endpoints/admin.cpp
@@ -106,4 +106,37 @@ void register_admin_handlers(App *app, pqxx::connection *db_conn) {
         resp["status"] = "ok";
         return crow::response(resp.dump());
     });
+
+    // Delete user
+    CROW_ROUTE((*app), "/delete_user").methods(crow::HTTPMethod::POST)
+    .CROW_MIDDLEWARES((*app), AdminSecMiddleware)([db_conn](const crow::request& req){
+        json data;
+        try {
+            data = json::parse(req.body);
+        } catch (...) {
+            return crow::response(400, gen_error_json("bad_request", "Failed to parse body!"));
+        }
+        if (!data.contains("user_id")) {
+            return crow::response(400, gen_error_json("bad_request", "User ID is missing!"));
+        }
+
+        long user_id = data["user_id"];
+        pqxx::work db_work(*db_conn);
+        try {
+            // Check for user existance
+            pqxx::row row = db_work.exec1(std::format("SELECT * FROM users WHERE id = '{}'", user_id));
+            // Update data
+            db_work.exec0(std::format("DELETE FROM users WHERE id = {}", user_id));
+            db_work.commit();
+        } catch (const pqxx::unexpected_rows& e) {
+            return crow::response(400, gen_error_json("user_not_registered", "This user is not registered!"));
+        } catch (const std::exception& e) {
+            CROW_LOG_ERROR << "An error while using DB has occured!\n" << e.what();
+            return crow::response(500);
+        }
+
+        json resp;
+        resp["status"] = "ok";
+        return crow::response(resp.dump());
+    });
 }
